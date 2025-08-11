4338e3f9cfae51c8159b806a52b66c788b960d02
Daniel Lindestad <lindestad@proton.me>
2025-08-11 23:33:00 +0200

    Update docker config to properly hot reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    host: '0.0.0.0',
+    watch: {
+      // Using polling improves reliability with Docker volume mounts on Windows (dev only)
+      usePolling: true,
+      interval: 200
+    }
   }
 })
